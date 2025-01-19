840d21760288b8688eb288b264ccb71e1799cfdd
Piod Gamer <malickelhadji07@gmail.com>
2025-01-19 17:32:35 +0000

    new push

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Carousel } from 'flowbite-react';
-import image1 from "../assets/image 1.avif";
-import image2 from "../assets/image 2.jpg";
-import image3 from "../assets/image3.webp";
-import image4 from "../assets/image4.jpg";
+import image1 from "../assets/1.1.webp";
+import image2 from "../assets/1.2.webp";
+import image3 from "../assets/1.3.webp";
+import image4 from "../assets/4.jpg";
+import image5 from "../assets/5.jpg";
+import image6 from "../assets/6.jpg";
+import image7 from "../assets/7.jpg";
 
 const Slide = () => {
     return (
@@ -13,6 +16,9 @@ const Slide = () => {
                 <img src={image2} alt="..." />
                 <img src={image3} alt="..." />
                 <img src={image4} alt="..." />
+                <img src={image5} alt="..." />
+                <img src={image6} alt="..." />
+                <img src={image7} alt="..." />
             </Carousel>
         </div>
     );
